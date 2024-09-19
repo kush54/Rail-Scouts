be91938833c846ddcc8ba863d0791ef2906b650d
kush54 <115143746+kush54@users.noreply.github.com>
2024-09-19 08:26:11 -0700

    second commit

--- index.js
+++ index.js
@@ -1,54 +1,142 @@
 
 
+const coords = { x: 0, y: 0 };
+const circles = document.querySelectorAll(".circles");
+
+const colors = [
+  "#ffb56b",
+  "#fdaf69",
+  "#f89d63",
+  "#f59761",
+  "#ef865e",
+  "#ec805d",
+  "#e36e5c",
+  "#df685c",
+  "#d5585c",
+  "#d1525c",
+  "#c5415d",
+  "#c03b5d",
+  "#b22c5e",
+  "#ac265e",
+  "#9c155f",
+  "#950f5f",
+  "#830060",
+  "#7c0060",
+  "#680060",
+  "#60005f",
+  "#48005f",
+  "#3d005e",
+  "#fff"
+];
+
+const scroll = new LocomotiveScroll({
+  el: document.querySelector('[data-scroll-container]'),
+  smooth: true
+});
+
+circles.forEach(function (circle, index) {
+  circle.x = 0;
+  circle.y = 0;
+//   circle.style.backgroundColor = colors[index % colors.length];
+});
+
+window.addEventListener("mousemove", function(e){
+  coords.x = e.clientX -10;
+  coords.y = e.clientY - 10;
+  
+});
+
+function animateCircles() {
+  
+  let x = coords.x;
+  let y = coords.y;
+  
+  circles.forEach(function (circle, index) {
+    circle.style.left = x - 12 + "px";
+    circle.style.top = y - 12 + "px";
+    
+    circle.style.scale = (circles.length - index) / circles.length;
+    
+    circle.x = x;
+    circle.y = y;
+
+    const nextCircle = circles[index + 1] || circles[0];
+    x += (nextCircle.x - x) * 0.3;
+    y += (nextCircle.y - y) * 0.3;
+  });
+ 
+  requestAnimationFrame(animateCircles);
+}
+
+animateCircles();
+
+
+
+
+
+var cursor = document.querySelector(".cursor")
+
+document.addEventListener("mousemove",function (e) {
+  blurcursor.style.left = e.x-150+"px"
+  blurcursor.style.top = e.y-150+"px"
+})
+
+
+
 
 var tl = gsap.timeline()
 
 
-tl.to(".trainImage",{
-  opacity: 1,
-  duration:1,
-  scale:1.2
-})
-
-tl.fromTo(".rsmobile,.rslaptop", 
-  { height: 0 }, // Starting height
-  { height: "250px", duration: 5 } // Ending height with increased duration
-);
-
-tl.to(".mdiv",{
-  y:-700,
-  duration:1,
-  opacity:0
-})
 
 
 
-setTimeout(() => {
-   var page0 = document.querySelector(".page0")
-   var page1 = document.querySelector(".page1")
-   var page2 = document.querySelector(".page2")
-   var page3 = document.querySelector(".page3")
-   var page4 = document.querySelector(".page4")
-   var pageextra = document.querySelector(".pageextra")
-   page0.style.display="none"
-   page1.style.display="block"
-   page2.style.display="block"
-  //  page3.style.display="block"
-   page4.style.display="block"
-   const isMobile = window.matchMedia("(max-width: 600px)").matches;
-            console.log(isMobile)
-            if (!isMobile) {
-              console.log("comp")
-              page3.style.display="block"
-          } else {
-              console.log("mob")
-              pageextra.style.display="block"
-          }
+// tl.to(".trainImage",{
+//   opacity: 1,
+//   duration:0.5,
+//   scale:1.2
+// })
+
+// tl.fromTo(".rsmobile, .rslaptop", 
+//   { height: 0 }, // Starting height
+//   { height: "550px", duration: 2 } // Ending height with decreased duration
+// );
+
+
+// tl.to(".mdiv",{
+//   y:-700,
+//   duration:1,
+//   opacity:0
+// })
+
+
+
+// setTimeout(() => {
+//    var page0 = document.querySelector(".page0")
+//    var page1 = document.querySelector(".page1")
+//    var page2 = document.querySelector(".page2")
+//    var page3 = document.querySelector(".page3")
+//    var page4 = document.querySelector(".page4")
+//    var pageextra = document.querySelector(".pageextra")
+//    page0.style.display="none"
+//    page1.style.display="block"
+//    page2.style.display="block"
+//   //  page3.style.display="block"
+//    page4.style.display="block"
+//    const isMobile = window.matchMedia("(max-width: 600px)").matches;
+//             console.log(isMobile)
+//             if (!isMobile) {
+//               console.log("comp")
+//               page3.style.display="block"
+//           } else {
+//               console.log("mob")
+//               pageextra.style.display="block"
+//           }
      
-}, 7000);
+// }, 3000);
 
 tl.from(".page1",{
-  scale:0
+  scale:0,
+  delay:-0.5
 })
 
 tl.from(".textlogo",{
@@ -82,7 +170,6 @@ tl.from(".developer",{
 })
 
 tl.from(".left",{
-  // x:-1000
   opacity:0,
   scale:0
 })
@@ -93,27 +180,25 @@ tl.from(".left-text",{
 
 tl.from(".locationImage",{
   opacity:0,
-  x:1000,
-  duration:1,
-  rotateY:720
+  x:-400,
 })
 
-const imageContainer = document.querySelector('.locationImage');
+// const imageContainer = document.querySelector('.re');
 
-        document.addEventListener('mousemove', (event) => {
-            const { clientX: x, clientY: y } = event;
-            const { innerWidth: width, innerHeight: height } = window;
+//         document.addEventListener('mousemove', (event) => {
+//             const { clientX: x, clientY: y } = event;
+//             const { innerWidth: width, innerHeight: height } = window;
 
-            const centerX = width / 2;
-            const centerY = height / 2;
+//             const centerX = width / 2;
+//             const centerY = height / 2;
 
-            // Calculate rotation values based on mouse position
-            const rotateY = ((x - centerX) / centerX) * 30; // Rotate around Y-axis
-            const rotateX = -((y - centerY) / centerY) * 30; // Rotate around X-axis
+//             // Calculate rotation values based on mouse position
+//             const rotateY = ((x - centerX) / centerX) * 10; // Rotate around Y-axis
+//             const rotateX = -((y - centerY) / centerY) * 10; // Rotate around X-axis
 
-            // Apply the transformation
-            imageContainer.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
-        });
+//             // Apply the transformation
+//             imageContainer.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`;
+//         });
 
 
       
@@ -122,4 +207,29 @@ const imageContainer = document.querySelector('.locationImage');
      
 
      
-         
+// Shery.imageMasker(".locationImage" /* Element to target.*/, {
+//   //Parameters are optional.
+//   mouseFollower: true,
+//   text: "Shery",
+//   ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+//   duration: 1,
+// });
+
+Shery.imageEffect(".images", {
+  style: 3,
+  /*optional parameters
+  these parameter dose not applies to custom scroll trigger callback */
+  scrollSnapping: true,
+  scrollSpeed: 6,
+  touchSpeed: 6,
+  damping: 7,
+});
+
+
+document.querySelector('.developer-container').addEventListener('click', () => {
+  window.scrollTo({
+      top: document.querySelector('.developer').offsetTop,
+      behavior: 'smooth'
+  });
+});
+
